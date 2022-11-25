88bb8e3a3132713be059800141f4a96cea3b41bf
Pau RE <prodrigestivill@gmail.com>
2022-11-25 11:42:19 +0100

    Fix backup directory issue on save

--- jmeter-wrapper.sh
+++ jmeter-wrapper.sh
@@ -12,6 +12,7 @@ JMETER_CONFIG="${XDG_CONFIG_HOME}/jmeter/user.properties"
 JMETER_CLASSPATH="${XDG_DATA_HOME}/jmeter/lib"
 JMETER_LIBEXT="${JMETER_CLASSPATH}/ext"
 JMETER_LOGFILE="${XDG_STATE_HOME}/jmeter/jmeter.log"
+JMETER_BACKUPS="${XDG_STATE_HOME}/jmeter/backups"
 
 mkdir -p "$JMETER_CLASSPATH" "$JMETER_LIBEXT" \
   "${XDG_CONFIG_HOME}/jmeter" \
@@ -26,6 +27,7 @@ exec /app/bin/jmeter \
   -J "help.local=true" \
   -J "search_paths=${JMETER_LIBEXT}" \
   -J "user.classpath=${JMETER_CLASSPATH}" \
+  -J "jmeter.gui.action.save.backup_directory=${JMETER_BACKUPS}" \
   -j "${JMETER_LOGFILE}" \
   -p "${JMETER_CONFIG}" \
   $@
